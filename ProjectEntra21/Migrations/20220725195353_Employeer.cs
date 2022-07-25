aadcfadfb4ad3207427298e3d0915085d3572956
Paulo Gileade <paulogileade22@gmail.com>
2022-07-25 16:55:15 -0300

    add migration employeer

---
+++ ProjectEntra21/Migrations/20220725195353_Employeer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectEntra21.Migrations
+{
+    public partial class Employeer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Funcionario",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    matricula = table.Column<long>(type: "bigint", nullable: false),
+                    nome = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    documento = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    data_nascimento = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    funcao = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    nivel_funcionario = table.Column<int>(type: "int", nullable: false),
+                    criado_em = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ultima_modificao_em = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Funcionario", x => x.id);
+                    table.UniqueConstraint("AK_Funcionario_matricula", x => x.matricula);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Funcionario");
+        }
+    }
+}
